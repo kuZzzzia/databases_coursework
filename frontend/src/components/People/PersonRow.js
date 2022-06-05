929dda1a7e20767c294911621d9b72426ed76cca
Maksim Kuzovchikov <mkuzovchikov@gmail.com>
2022-06-05 14:47:28 +0300

    Bug fix with naming

--- frontend/src/components/People/PersonRow.js
+++ frontend/src/components/People/PersonRow.js
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
 
 const PersonRow = (props) => {
-    const photo = props.person.image;
-    const name = props.person.name;
-    const altName = props.person.altName;
-    const date = props.person.date;
+    const photo = props.person.Image;
+    const name = props.person.Name;
+    const altName = props.person.AltName;
+    const date = props.person.Date;
+    const id = props.person.ID;
 
     return (
         <div className="card mb-5 pb-2">
@@ -13,7 +14,7 @@ const PersonRow = (props) => {
                 <h5 className="card-title">{name}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">{altName}</h6>
                 <p className="card-text">Дата рождения: {date}</p>
-                <Link className="card-link-link" to="/auth">View more</Link>
+                <Link className="card-link-link" to="/films/:"{id}>View more</Link>
             </div>
         </div>
     );
